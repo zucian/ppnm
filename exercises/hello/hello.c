1c5e78b98751380aad9a0e69835c9cf70224cb2d
Niels Mikkelsen <zucianniels@gmail.com>
2021-02-12 14:03:53 +0100

    added makefile with hello world example

---
+++ exercises/hello/hello.c
@@ -0,0 +1,7 @@
+#include "stdio.h" // for html display only, you should use angular brackets
+#include "math.h"  // for html display only, you should use angular brackets
+int main(void){
+	double r=sqrt(2.0);
+	printf("hello, sqrt(2)=%g\n",r);
+return 0;
+}
