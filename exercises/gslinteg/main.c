b3a702fd074c49d3c64abb9b65dd98478a380f52
Niels Mikkelsen <zucianniels@gmail.com>
2021-03-14 22:44:56 +0100

    Made part A of integration exercise

---
+++ exercises/gslinteg/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_integration.h>
+
+double Afunction(double x, void* params){
+    double value = log(x)/(sqrt(x));
+    return value;
+}
+
+double aIntegral(){
+    gsl_function F;
+    F.function = &Afunction;
+    int limit = 999;
+    gsl_integration_workspace* w = gsl_integration_workspace_alloc(limit);
+    double a=0, b=1, acc=1e-6, eps=1e-6, result, error;
+    gsl_integration_qags(&F, a, b, acc, eps, limit, w, &result, &error);
+    gsl_integration_workspace_free(w);
+    return result;
+}
+
+int main(){
+    FILE* outA = fopen("out.valueExerciseA.txt","w");
+    double exerciseA = aIntegral();
+    fprintf(outA, "int(log(x)/sqrt(x), x=0..1) = %g\n", exerciseA);
+    return 0;
+}
--- exercises/plots/main.c
+++ exercises/plots/main.c
@@ -3,14 +3,14 @@
 #include <gsl/gsl_sf_erf.h>
 #include <gsl/gsl_sf_gamma.h>
 
-double Erf(double);
-double Gamma(double);
+double myErf(double);
+double myGamma(double);
 
 int main(){
     double xmin=-2.1, xmax=4.2;
     double x=xmin;
     while(x<=xmax){
-        printf("%10g %10g %10g %10g %10g %10g %10g\n",x, erf(x), gsl_sf_erf(x), Erf(x),tgamma(x), gsl_sf_gamma(x), Gamma(x));
+        printf("%10g %10g %10g %10g %10g %10g %10g\n",x, erf(x), gsl_sf_erf(x), myErf(x),tgamma(x), gsl_sf_gamma(x), myGamma(x));
         x+=1.0/8;
     }
 
