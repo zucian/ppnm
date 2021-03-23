24ef905ece66b50f2298702562407dc3e25aa941
Niels Mikkelsen <zucianniels@gmail.com>
2021-03-23 16:14:07 +0100

    Continued working on part A of least_squares

--- homework/least_squares/main.c
+++ homework/least_squares/main.c
@@ -5,16 +5,27 @@
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_vector.h>
 
-void least_sqr_fit(gsl_matrix* data_matrix, f functions[], gsl_vector* c){
-    gsl_matrix* A = gsl_matrix_alloc(data_matrix->size1,function_output);
-    gsl_vector* y = gsl_vector_alloc(data_matrix->size1);
-    gsl_matrix_get_col(y, data_matrix, 1);
+typedef double (*f)(double x);
+
+double f_0(double x){
+    return 1.;
+}
+
+double f_1(double x){
+    return x;
+}
+
+void least_sqr_fit(gsl_matrix* dataMatrix, f functions[],int nFunctions, gsl_vector* c){
+    gsl_matrix* A = gsl_matrix_alloc(dataMatrix->size1,nFunctions);
+    gsl_matrix* R = gsl_matrix_alloc(dataMatrix->size1,nFunctions);
+    gsl_vector* y = gsl_vector_alloc(dataMatrix->size1);
+    gsl_matrix_get_col(y, dataMatrix, 1);
 
     for(int i=0; i<(A->size1); i++){
         double x_i = gsl_matrix_get(data_matrix,i,0); //Gets the i'th x-value and stores it in x_i
         for (int j=0; j<(A->size2); j++){
             double A_ij = functions[j](x_i); //Computes f_j(x_i) and stores it in A_ij
-            gsl_matrix_set(A,i,j,A_ij);
+            gsl_matrix_set(A,i,j,A_ij); //Set (i,j) in A equal to A_ij
         }
     }
     GS_decomp(A,R); //Gram-schmidt storing orthogonal Q in A and R is upper triangular
@@ -22,11 +33,26 @@ void least_sqr_fit(gsl_matrix* data_matrix, f functions[], gsl_vector* c){
 
     gsl_matrix_free(A);
     gsl_matrix_free(R);
+    gsl_vector_free(y);
 }
 
 int main(){
-    double x[] = {1., 2., 3., 4., 6., 9., 10., 13., 15.};
-    double y[] = {117., 100., 88., 72.,  53., 29.5, 25.2, 15.2, 11.1};
+    int nData = 9;
+    int nFunctions = 2;
+    double x[nData] = {1., 2., 3., 4., 6., 9., 10., 13., 15.};
+    double y[nData] = {117., 100., 88., 72.,  53., 29.5, 25.2, 15.2, 11.1};
+
+    gsl_matrix* dataMatrix = gsl_matrix_alloc(nData,3);
+    gsl_vector* c = gsl_vector_alloc(nData);
+    for(int i=0; i<nDatapoints; i++){
+        gsl_matrix_set(dataMatrix,i,0,x[i]); //Set first column in dataMatrix equal to x-values
+        gsl_matrix_set(dataMatrix,i,1,log(y[i])); //Set second column in dataMatrix equal to log of y-values
+        gsl_matrix_set(dataMatrix,i,2,(y[i]/20)/y[i]); //Set third column in dataMatrix equal to uncertainty in y
+    }
+
+    functions[2] = {&f0, &f1};
+
+    least_sqr_fit(dataMatrix,functions,nFunctions,c);
 
     return 0;
 }
