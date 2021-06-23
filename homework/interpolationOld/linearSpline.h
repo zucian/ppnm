b81ab7eee8e7bf5b3b1b315d026e064d89cb615b
Niels Mikkelsen <zucianniels@gmail.com>
2021-06-23 22:09:06 +0200

    Changed directory name

--- homework/interpolationOld/linearSpline.h
+++
@@ -1,8 +0,0 @@
-#ifndef HAVE_LINEARSPLINE_H
-#define HAVE_LINEARSPLINE_H
-
-double linear_spline(int numberOfPoints, double *points, double *functionValues, double evaluationPoints);
-
-double linear_spline_integrate(int numberOfPoints, double *points, double *functionValues, double evaluationPoint);
-
-#endif
